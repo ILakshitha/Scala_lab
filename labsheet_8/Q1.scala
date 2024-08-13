b92136a3e40430788c3abf7e50026841a7059626
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-08-13 09:11:40 +0530

    complete code

--- labsheet_8/Q1.scala
+++ labsheet_8/Q1.scala
@@ -2,38 +2,43 @@ def caesar_encrypt(plaintext: String, shift: Int) = {
   plaintext.map { char =>
     if (char.isLetter) {
       val ascioffset = if (char.isUpper) 'A' else 'a'
-      ((char - ascioffset + shift) % 26 + ascioffset).toChar
-    } else{
-        char
+      if ((char - ascioffset + shift)<0){
+      ((char - ascioffset + shift) % 26 + ascioffset + 26).toChar
+      }else{
+         ((char - ascioffset + shift) % 26 + ascioffset ).toChar
+      }
+    } else {
+      char
     }
   }
 }
 
-
-def caesar_decrypt(text:String,shift:Int):String ={
-    caesar_encrypt(text, -shift)
+def caesar_decrypt(text: String, shift: Int): String = {
+  caesar_encrypt(text, -shift)
 }
 
 def caesarCipher(text: String, shift: Int, mode: String): String = {
   mode match {
     case "encrypt" => caesar_encrypt(text, shift)
     case "decrypt" => caesar_decrypt(text, shift)
-    case _ => throw new IllegalArgumentException("Mode should be either 'encrypt' or 'decrypt'.")
+    case _ =>
+      throw new IllegalArgumentException(
+        "Mode should be either 'encrypt' or 'decrypt'."
+      )
   }
 }
 
+def main(args: Array[String]): Unit = {
 
-def main(args: Array[String]):Unit={
-
-val plaintext = "Hello, World!"
-val shift = 3
+  val plaintext = "abc"
+  val shift = 3
 
 // Encrypt the plaintext
-val encryptedText = caesarCipher(plaintext, shift, "encrypt")
-println(s"Encrypted: $encryptedText")
+  val encryptedText = caesarCipher(plaintext, shift, "encrypt")
+  println(s"Encrypted: $encryptedText")
 
 // Decrypt the ciphertext
-val decryptedText = caesarCipher(encryptedText, shift, "decrypt")
-println(s"Decrypted: $decryptedText")
+  val decryptedText = caesarCipher(encryptedText, shift, "decrypt")
+  println(s"Decrypted: $decryptedText")
 
 }
---
+++ labsheet_8/Q2.scala
@@ -0,0 +1,17 @@
+import scala.io.StdIn
+
+object NumberCategorizer {
+  def main(args: Array[String]): Unit = {
+    println("Please enter an integer: ")
+    val input = StdIn.readInt()
+
+    val categorize: Int => String = {
+      case n if n % 3 == 0 && n % 5 == 0 => "Multiple of Both Three and Five"
+      case n if n % 3 == 0               => "Multiple of Three"
+      case n if n % 5 == 0               => "Multiple of Five"
+      case _                             => "Not a Multiple of Three or Five"
+    }
+
+    println(categorize(input))
+  }
+}
